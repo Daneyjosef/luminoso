7121c2dd21478d76d1035050cd0cceb1dbc787d4
unknown <Daniel@HICT.mainserverDC.solng>
2026-06-03 11:29:16 +0100

    Remove Our Services section from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import HeroSection from '@/components/sections/HeroSection';
 import AboutSection from '@/components/sections/AboutSection';
-import ServicesSection from '@/components/sections/ServicesSection';
 import PortfolioPreviewSection from '@/components/sections/PortfolioPreviewSection';
 import ContactSection from '@/components/sections/ContactSection';
 
@@ -11,7 +10,6 @@ export default function Home() {
     <main className="overflow-hidden">
       <HeroSection />
       <AboutSection />
-      <ServicesSection />
       <PortfolioPreviewSection />
       <ContactSection />
     </main>
